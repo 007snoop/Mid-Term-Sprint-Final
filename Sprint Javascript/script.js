bade588a268a0fe95f254a0d823a6e7c4954d2c0
Colin Yetman <colinguitarist@gmail.com>
2024-08-04 18:52:07 -0230

    display js to webpage

--- Sprint Javascript/script.js
+++ Sprint Javascript/script.js
@@ -9,10 +9,22 @@ fetch("JSON/games.json")
 
 fetch("JSON/person.json")
     .then(response => response.json())
+
     .then(data => {
+
+        const peopleContainer = document.createElement("div");
+        peopleContainer.id = "people";
+
         console.log(data);
+
         data.forEach((person) => {
-            console.log(`${person.fName} ${person.lName}`)
+            const personItem = document.createElement("li");
+            personItem.textContent = `${person.fName} ${person.lName}`
+
+            peopleContainer.appendChild(personItem);
+
+   
         });
+        document.body.appendChild(peopleContainer);
     })
 
